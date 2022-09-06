4dca371479c6f4d7b06a466641756f0524aebf57
James Wild <wild.james343@gmail.com>
2022-09-06 18:57:06 +0100

    Better tests

--- tests/test_possible_moves.py
+++ tests/test_possible_moves.py
@@ -1,18 +1,33 @@
+import numpy as np
 import pytest
 from src.knights_journey import get_possible_moves
 
-
 possible_moves_test_io = [
     [
-        [0, 0],
-        [
+        np.array([3, 3]),
+        np.array(
+            [
+                [4, 5],
+                [5, 4],
+                [5, 2],
+                [4, 1],
+                [2, 1],
+                [1, 2],
+                [1, 4],
+                [2, 5],
+            ]
+        ),
+    ],
+    [
+        np.array([0, 0]),
+        np.array([
             [1, 2],
             [2, 1],
-        ],
+        ]),
     ]
 ]
 
 
 @pytest.mark.parametrize("position,expected", possible_moves_test_io)
 def test_possible_moves(position, expected):
-    assert get_possible_moves(position) == expected
+    assert np.array_equal(get_possible_moves(position), expected)
