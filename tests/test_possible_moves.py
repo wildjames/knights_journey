afede03091ceef7099516e84532197a780d8a4d7
James Wild <wild.james343@gmail.com>
2022-09-06 20:14:33 +0100

    Test notation conversions

--- tests/test_possible_moves.py
+++ tests/test_possible_moves.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from src.knights_journey import get_possible_moves
+from src.knights_journey import chess_notation_to_index, get_possible_moves, index_to_chess_notation
 
 possible_moves_test_io = [
     [
@@ -45,3 +45,32 @@ possible_moves_test_io = [
 @pytest.mark.parametrize("position,expected", possible_moves_test_io)
 def test_possible_moves(position, expected):
     assert np.array_equal(get_possible_moves(position), expected)
+
+
+positions = [
+    [
+        np.array([0,0]),
+        "A1"
+    ],
+    [
+        np.array([1,0]),
+        "B1"
+    ],
+    [
+        np.array([0,1]),
+        "A2"
+    ],
+    [
+        np.array([7,7]),
+        "H8"
+    ]
+]
+
+@pytest.mark.parametrize("position,index", positions)
+def test_index_to_chess_notation(position, index):
+    assert index_to_chess_notation(position) == index
+
+
+@pytest.mark.parametrize("position,index", positions)
+def test_chess_notation_to_index(position, index):
+    assert np.array_equal(chess_notation_to_index(index), position)
