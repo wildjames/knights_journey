fcc67fdcb2dde39f9ef7e8e26d9b88cd4ace842c
James Wild <wild.james343@gmail.com>
2022-09-06 22:40:01 +0100

    Alter tests for new function

--- tests/test_possible_moves.py
+++ tests/test_possible_moves.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from src.knights_journey import chess_notation_to_index, get_possible_moves, index_to_chess_notation, move_between_positions_DFS
+from src.knights_journey import *
 
 possible_moves_test_io = [
     [
@@ -20,25 +20,31 @@ possible_moves_test_io = [
     ],
     [
         [0, 0],
-        np.array([
-            [1, 2],
-            [2, 1],
-        ]),
+        np.array(
+            [
+                [1, 2],
+                [2, 1],
+            ]
+        ),
     ],
     [
         [7, 7],
-        np.array([
-            [6, 5],
-            [5, 6],
-        ]),
+        np.array(
+            [
+                [6, 5],
+                [5, 6],
+            ]
+        ),
     ],
     [
         [0, 7],
-        np.array([
-            [2, 6],
-            [1, 5],
-        ]),
-    ]
+        np.array(
+            [
+                [2, 6],
+                [1, 5],
+            ]
+        ),
+    ],
 ]
 
 
@@ -47,24 +53,8 @@ def test_possible_moves(position, expected):
     assert np.array_equal(get_possible_moves(position), expected)
 
 
-positions = [
-    [
-        [0,0],
-        "A1"
-    ],
-    [
-        [1,0],
-        "B1"
-    ],
-    [
-        [0,1],
-        "A2"
-    ],
-    [
-        [7,7],
-        "H8"
-    ]
-]
+positions = [[[0, 0], "A1"], [[1, 0], "B1"], [[0, 1], "A2"], [[7, 7], "H8"]]
+
 
 @pytest.mark.parametrize("position,index", positions)
 def test_index_to_chess_notation(position, index):
@@ -79,25 +69,41 @@ def test_chess_notation_to_index(position, index):
 test_pathfinding = [
     [
         # One move gets you there
-        [[0,0],[1, 2]],
-        2
+        [[0, 0], [1, 2]],
+        2,
     ],
     [
         # Two moves gets you there
-        [[0,0],[2, 4]],
-        3
+        [[0, 0], [2, 4]],
+        3,
     ],
     [
-        # Two moves gets you there
-        [[0,0],[7, 7]],
-        10
-    ],   
+        # Cross the board
+        [[0, 0], [7, 7]],
+        7,
+    ],
+    [
+        # And back again
+        [[7, 7], [0, 0]],
+        7,
+    ],
+    [
+        # From near the middle
+        [[5, 3], [7, 7]],
+        4,
+    ],
+    [
+        # Traverse in a straight line
+        [[4, 0], [4, 7]],
+        6,
+    ],
 ]
+
 
 @pytest.mark.parametrize("input,output", test_pathfinding)
 def test_pathfinding(input, output):
     start = input[0]
     stop = input[1]
 
-    n_moves = len(move_between_positions_DFS(start, stop)) 
+    n_moves = len(move_between_positions(stop, [[start]]))
     assert output >= n_moves
