45679c2c74712e40fb39b4f1ef0edf8092da43a1
James Wild <wild.james343@gmail.com>
2022-09-07 22:43:58 +0100

    I shouldnt code this late - actually do stdin properly.

--- endless_knight.py
+++ endless_knight.py
@@ -1,10 +1,10 @@
 #!python3
 
-from src.knights_workers import print_path
 import sys
 
+from src.knights_workers import print_path
 
 if __name__ in "__main__":
-    for line in sys.stdin.readlines():
+    for line in sys.stdin:
         start, stop = line.strip().split(" ")
         print_path(start, stop)
